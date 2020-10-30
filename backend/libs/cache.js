a1e94448c57c05be7b2c9aa81fedd0d620a6b5fe
Takeshi Yasunaga <yasunaga@haw.co.jp>
2020-10-30 19:15:50 +0900

    cache refactoring

--- backend/libs/cache.js
+++ backend/libs/cache.js
@@ -4,144 +4,83 @@ const tapyrusd = require('../libs/tapyrusd').client;
 const electrs = require('../libs/electrs');
 const logger = require('../libs/logger');
 
-async function getBlockchainInfo() {
-  const result = await tapyrusd.getBlockchainInfo();
-  return result.headers;
-}
-
-async function getBlockWithTx(blockNum) {
-  const blockHash = await tapyrusd.getBlockHash(blockNum);
-  const result = await tapyrusd.getBlock(blockHash);
-  return result;
-}
-
-const createCache = function () {
-  return new Promise((resolve, reject) => {
-    try {
-      const cache = flatCache.load(
-        'transactionCache',
-        path.resolve('./tmp/cache')
-      );
-
-      getBlockchainInfo().then(async bestBlockHeight => {
-        let count = 0;
-        let cacheBestBlockHeight = cache.getKey(`bestBlockHeight`);
-        //bestBlockHeight = bestBlockHeight - 40000;
-
-        if (!cacheBestBlockHeight) cacheBestBlockHeight = 0;
-        else if (cacheBestBlockHeight >= bestBlockHeight) {
-          logger.info('Cache is up-to-date');
-          return resolve();
-        } else {
-          cacheBestBlockHeight++;
-          count = cache.getKey(`transactionCount`);
-        }
-        while (cacheBestBlockHeight <= bestBlockHeight) {
-          logger.info('current block height: ', cacheBestBlockHeight);
-          const block = await getBlockWithTx(cacheBestBlockHeight);
-
-          for (let i = 0; i < block.nTx; i++) {
-            cache.setKey(`${count++}`, block.tx[i]);
-
-            await electrs.blockchain.transaction
-              .get(block.tx[i], true)
-              .then(async response => {
-                const responses = [response];
-
-                for (var vin of responses[0].vin) {
-                  if (vin.txid) {
-                    await electrs.blockchain.transaction
-                      .get(vin.txid, true)
-                      .then(response => {
-                        const vinResponses = [response];
-
-                        for (let vout of vinResponses[0].vout) {
-                          for (let address of vout.scriptPubKey.addresses) {
-                            //flag to represent the availability of this address in the vout of original Transaction
-                            let isPresent = false;
-
-                            for (let originalVout of responses[0]['vout']) {
-                              //avoiding multiple entries of the same address for transaction cache
-                              if (
-                                originalVout.scriptPubKey.addresses &&
-                                originalVout.scriptPubKey.addresses.indexOf(
-                                  address
-                                )
-                              ) {
-                                isPresent = true;
-                                break;
-                              }
-                            }
-
-                            if (isPresent) {
-                              continue;
-                            }
-
-                            let addressTxCount = cache.getKey(
-                              `${address}_count`
-                            );
-
-                            if (!addressTxCount && addressTxCount !== 0)
-                              addressTxCount = -1;
-
-                            addressTxCount++;
-                            cache.setKey(
-                              `${address}_${addressTxCount}`,
-                              block.tx[i]
-                            );
-                            cache.setKey(`${address}_count`, addressTxCount);
-                            cache.save(true /* noPrune */);
-                          }
-                        }
-                      });
-                  }
-                }
-
-                for (let vout of responses[0]['vout']) {
-                  if (vout.scriptPubKey.addresses) {
-                    for (let address of vout.scriptPubKey.addresses) {
-                      let addressTxCount = cache.getKey(`${address}_count`);
-
-                      if (!addressTxCount && addressTxCount !== 0)
-                        addressTxCount = -1;
-
-                      addressTxCount++;
-                      cache.setKey(`${address}_${addressTxCount}`, block.tx[i]);
-                      cache.setKey(`${address}_count`, addressTxCount);
-
-                      let addressReceived = cache.getKey(`${address}_received`);
-                      if (!addressReceived) addressReceived = 0;
-
-                      addressReceived += vout.value;
-                      cache.setKey(`${address}_received`, addressReceived);
-
-                      cache.save(true /* noPrune */);
-                    }
-                  }
-                }
-              });
-          }
-
-          cacheBestBlockHeight++;
-        }
-
-        cache.setKey('bestBlockHeight', bestBlockHeight);
-        cache.setKey('transactionCount', count);
-
-        logger.info('Updated cache till block height -> ', bestBlockHeight);
-        logger.info('New transaction count -> ', count);
-
-        cache.save(true /* noPrune */);
-        return resolve();
-      });
-    } catch (err) {
-      return reject(err);
-    }
-  });
-};
-
 const loadCache = () => {
   return flatCache.load('transactionCache', path.resolve('./tmp/cache'));
+};
+
+const createCache = async () => {
+  const bestBlockHeight = await tapyrusd.getBlockCount();
+
+  const cache = loadCache();
+  let cacheBestBlockHeight = cache.getKey(`bestBlockHeight`) || 0;
+  if (cacheBestBlockHeight >= bestBlockHeight) {
+    logger.info('Cache is up-to-date');
+    return;
+  }
+
+  cacheBestBlockHeight++;
+  let transactionCount = cache.getKey('transactionCount') || 0;
+
+  while (cacheBestBlockHeight <= bestBlockHeight) {
+    logger.info('current block height: ', cacheBestBlockHeight);
+
+    const blockHash = await tapyrusd.getBlockHash(cacheBestBlockHeight);
+    const block = await tapyrusd.getBlock(blockHash);
+
+    block.tx.forEach(async txid => {
+      cache.setKey(`${transactionCount++}`, txid);
+      const tx = electrs.blockchain.transaction.get(txid, true);
+      tx.vin.forEach(async vin => {
+        if (!vin.txid) return;
+
+        const vinTx = await electrs.blockchain.transaction.get(vin.txid, true);
+        vinTx.vout.forEach(vout => {
+          vout.scriptPubKey.addresses.forEach(address => {
+            // flag to represent the availability of this address in the vout of original Transaction
+            const addressIncluded = tx.vout.some(vout => {
+              vout.scriptPubKey.addresses &&
+                vout.scriptPubKey.addresses.includes(address);
+            });
+            if (addressIncluded) return;
+
+            let addressTxCount = cache.getKey(`${address}_count`) || -1;
+            addressTxCount++;
+
+            cache.setKey(`${address}_${addressTxCount}`, txid);
+            cache.setKey(`${address}_count`, addressTxCount);
+            cache.save(true /* noPrune */);
+          });
+        });
+      });
+
+      tx.vout.forEach(async vout => {
+        if (!vout.scriptPubKey.addresses) return;
+
+        vout.scriptPubKey.addresses.forEach(address => {
+          let addressTxCount = cache.getKey(`${address}_count`) || -1;
+          addressTxCount++;
+
+          cache.setKey(`${address}_${addressTxCount}`, txid);
+          cache.setKey(`${address}_count`, addressTxCount);
+
+          let addressReceived = cache.getKey(`${address}_received`) || 0;
+          addressReceived += vout.value;
+          cache.setKey(`${address}_received`, addressReceived);
+          cache.save(true /* noPrune */);
+        });
+      });
+    });
+
+    cacheBestBlockHeight++;
+  }
+
+  cache.setKey('bestBlockHeight', bestBlockHeight);
+  cache.setKey('transactionCount', transactionCount);
+
+  logger.info('Updated cache till block height -> ', bestBlockHeight);
+  logger.info('New transaction count -> ', transactionCount);
+
+  cache.save(true /* noPrune */);
 };
 
 module.exports = {
